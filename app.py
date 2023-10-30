8809c0f694c3d8e3092e0e985ecc562be6df8674
Almas Ansari <itsmealmas.ansari@gmail.com>
2023-10-30 17:54:21 +0000

    first push

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ uploaded_files = st.file_uploader("Choose CSV or Excel files", accept_multiple_f
 
 if uploaded_files:
     file_names = [file.name for file in uploaded_files]
-    file_selection = st.selectbox("Select a file to analyze:", file_names)
+    selected_file_name = st.selectbox("Select a file to analyze:", file_names)
     if st.button('Generate Report'):
+        # Find the UploadedFile object that matches the selected file name
+        file_selection = next(file for file in uploaded_files if file.name == selected_file_name)
         if file_selection.type == 'text/csv':
             df = pd.read_csv(file_selection)
         elif file_selection.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
