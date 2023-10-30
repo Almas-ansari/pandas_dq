4fbb55539cc82d5896442fa0a3cb793e4f0693df
Almas Ansari <itsmealmas.ansari@gmail.com>
2023-10-30 17:46:32 +0000

    first push

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
 import streamlit as st
 import pandas as pd
 import pandas_dq
-import PyMuPDF
-import fitz
 
 def generate_report(df):
     report = pandas_dq.dq_report(df)
     st.write(report)  # Adjust this line based on how pandas_dq structures the report
 
-def process_pdf(file):
-    pdf_document = PyMuPDF.open(file)
-    text = ""
-    for page_num in range(len(pdf_document)):
-        page = pdf_document.loadPage(page_num)
-        text += page.getText()
-    pdf_document.close()
-    # Assume each line in the PDF is a new row in the DataFrame
-    rows = text.strip().split('\n')
-    # Assume the first row contains column headers
-    df = pd.DataFrame([row.split() for row in rows[1:]], columns=rows[0].split())
-    return df
+st.title('Data Report Generator')
 
-st.title('PDF Report Generator')
-
-uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type=['pdf'])
+uploaded_files = st.file_uploader("Choose CSV or Excel files", accept_multiple_files=True, type=['csv', 'xlsx'])
 
 if uploaded_files:
-    pdf_selection = st.selectbox("Select a PDF to analyze:", uploaded_files)
+    file_selection = st.selectbox("Select a file to analyze:", uploaded_files)
     if st.button('Generate Report'):
-        df = process_pdf(pdf_selection)
+        if file_selection.type == 'text/csv':
+            df = pd.read_csv(file_selection)
+        elif file_selection.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            df = pd.read_excel(file_selection)
         generate_report(df)
