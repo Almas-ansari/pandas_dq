d69dbba0c245b0319e369b9713cac359ce4840a6
Almas Ansari <itsmealmas.ansari@gmail.com>
2023-10-30 17:31:53 +0000

    first push

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import pandas_dq
+import PyMuPDF
+
+def generate_report(df):
+    report = pandas_dq.dq_report(df)
+    st.write(report)  # Adjust this line based on how pandas_dq structures the report
+
+def process_pdf(file):
+    pdf_document = PyMuPDF.open(file)
+    text = ""
+    for page_num in range(len(pdf_document)):
+        page = pdf_document.loadPage(page_num)
+        text += page.getText()
+    pdf_document.close()
+    # Assume each line in the PDF is a new row in the DataFrame
+    rows = text.strip().split('\n')
+    # Assume the first row contains column headers
+    df = pd.DataFrame([row.split() for row in rows[1:]], columns=rows[0].split())
+    return df
+
+st.title('PDF Report Generator')
+
+uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type=['pdf'])
+
+if uploaded_files:
+    pdf_selection = st.selectbox("Select a PDF to analyze:", uploaded_files)
+    if st.button('Generate Report'):
+        df = process_pdf(pdf_selection)
+        generate_report(df)
